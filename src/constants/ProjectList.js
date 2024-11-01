f745353108f7bcd1786d3f21bb2cc73d08a88dd4
Philip Cai <u7588991@anu.edu.au>
2024-11-02 00:50:36 +1100

    fix media path

--- src/constants/ProjectList.js
+++ src/constants/ProjectList.js
@@ -1,25 +1,31 @@
+import androidPhoto from "../../src/assets/media/HomePage.png"
+import posterPhoto from "../../src/assets/media/poster.png"
+import posterPDF from "../../src/assets/media/final_poster.pdf"
+import dsPhoto from "../../src/assets/media/ds_project.png"
+import dsPDF from "../../src/assets/media/ds_project.pdf"
+
 const projectList = [
     {
-      imageSrc: "../../src/assets/media/HomePage.png",
+      imageSrc: androidPhoto,
       title: "Android Project",
       description: "An Android app that offers exhaustive browsing capabilities for users to view and save their favourite games, while allowing users to connect and chat with friends.",
       buttonLabel: "App Demo",
       videoUrl: "https://www.youtube.com/embed/g0HyjkvVENo?si=GoHK9_1UpFHl55Np",
     },
     {
-      imageSrc: "../../src/assets/media/poster.png",
+      imageSrc: posterPhoto,
       title: "XAI Project",
       description: "A Python-based project that utilises Explainable AI (XAI) methods to provide insights for renewable energy stakeholders as to how (optimal) machine learning models reach certain decisions.",
       buttonLabel: "Project Pitch",
       videoUrl: "https://www.youtube.com/embed/Knzk_9TG-bk?si=9cY-5gQdcqWMuonF",
-      linkUrl: "../../src/assets/media/final_poster.pdf",
+      linkUrl: posterPDF,
     },
     {
-      imageSrc: "../../src/assets/media/ds_project.png",
+      imageSrc: dsPhoto,
       title: "Housing Prediction Project",
       description: "A data science project that uses web-scraping, RESTful API and machine learning to predict housing prices and assess real estate trends from extensive data.",
       buttonLabel: "GitHub Link",
-      linkUrl: "../../src/assets/media/ds_project.pdf",
+      linkUrl: dsPDF,
     }
   ];
   
